40f6bba7ef015ed30e62a163069c84f4487c5cfc
Lucas Salvador <lucassalvador948@gmail.com>
2025-01-13 23:02:09 -0300

    Update crianca.py
    criancises

--- crianca.py
+++ crianca.py
@@ -1,40 +1,54 @@
 from pessoa import Pessoa
 
-
 class Crianca(Pessoa):
-    def __init__(self, nome, sobrenome, idade, endereco, chamine, status_presentes, lista_ranking):
-        super.__init__(self, nome, sobrenome, idade, endereco)
+    def __init__(self, nome: str, sobrenome: str, idade, endereco: str, 
+                 chamine: bool, status_presentes: bool, lista_ranking: int):
+        
+        super().__init__(nome, sobrenome, idade, endereco)
 
-        self._ chamine = chamine
+        self._chamine = chamine
         self._status_presentes = status_presentes
         self._lista_ranking = lista_ranking
-
-    
-
 
     @property
     def chamine(self):
         return self._chamine
 
-    
     @chamine.setter
-    def chamine(self, chamnine):
-        self._chamine = chamnine
+    def chamine(self, valor: bool):
+        if isinstance(valor, bool):
+            self._chamine = valor
+        else:
+            raise ValueError("O atributo 'chamine' deve ser do tipo bool.")
 
-    
     @property
     def status_presentes(self):
         return self._status_presentes
 
     @status_presentes.setter
-    def status_presentes(self, status_presentes):
-        self._status_presentes = status_presentes
+    def status_presentes(self, valor: bool):
+        if isinstance(valor, bool):
+            self._status_presentes = valor
+        else:
+            raise ValueError("O atributo 'status_presentes' deve ser do tipo bool.")
 
-    
     @property
     def lista_ranking(self):
         return self._lista_ranking
 
     @lista_ranking.setter
-    def lista_ranking(self, lista_ranking):
-        self._lista_ranking = lista_ranking
+    def lista_ranking(self, valor: int):
+        if isinstance(valor, int):
+            self._lista_ranking = valor
+        else:
+            raise ValueError("O atributo 'lista_ranking' deve ser um número inteiro)
+        
+    # CTRL + K + C: Comentar
+    # CTRL + K + U: Descomentar
+
+    # def mais_boazinha(self):
+    #     #Num sei como fazer se vamos colocar um atributo pra isso, ou se vai involver a lista rank
+
+
+    # def mais_malcriada(self):
+    #     #The same thing

5bf0e5ab7c17ba9a3dfeb97e6a7401e31ada1dd7
Lucas Salvador <lucassalvador948@gmail.com>
2025-01-13 23:34:23 -0300

    Update claus.py

--- claus.py
+++ claus.py
@@ -1,19 +1,29 @@
+#Deixei esses raise value error assim por que é para debugg no codigo o usuario vai digitar s(sim) ou n(não)
+
+from presente import Presente
 from pessoa import Pessoa
 
-
 class Claus(Pessoa):
-    def __init__(self, nome, sobrenome, idade, endereco, licensa_treno):
-        super.__init__(self, nome, sobrenome, idade, endereco)
+    def __init__(self, nome: str, sobrenome: str, idade, endereco: str, licensa_treno: int):
+        super().__init__(nome, sobrenome, idade, endereco)
 
-        self.__licensa_treno = licensa_treno
+        self.licensa_treno = licensa_treno
 
-    
     @property
-    def get_licensa_treno(self):
-        return self.__licensa_treno
+    def licensa_treno(self):
+        return self._licensa_treno
 
-    @get_licensa_treno.setter
-    def get_licensa_treno(self, nova_licensa):
-        if not isinstance(nova_licensa, str):
-            raise ValueError("A licença de treino deve ser uma string.")
-        self.__licensa_treno = nova_licensa
+    @licensa_treno.setter
+    def licensa_treno(self, valor: int):
+        if isinstance(valor, int) and valor > 0:
+            self._licensa_treno = valor
+        else:
+            raise ValueError("A licença do trenó deve ser um número inteiro positivo.")
+
+    def presente_entregado(self, presente):
+
+        if isinstance(presente, Presente):
+            presente.presente_entregue = True
+            print(f"O presente '{presente.nome_presente}' foi entregue pelo Claus.")
+        else:
+            raise ValueError("O argumento deve ser uma instância da classe Presente.")

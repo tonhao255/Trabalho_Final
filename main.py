90beb2ceafe84b4c2589dd862f93c082a7c0dd27
Lucas Salvador <lucassalvador948@gmail.com>
2025-01-13 23:36:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ from pessoa import Pessoa
 from elfo import Elfo
 from claus import Claus
 from crianca import Crianca
+from presente import Presente
+
+while True:
+    break
+print("São 23:29, vou estudar por meia hora para biologia")

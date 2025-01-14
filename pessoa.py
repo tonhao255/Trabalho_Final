ef740219bce1ece6abcd57ab24d5cefbb32466dc
Lucas Salvador <lucassalvador948@gmail.com>
2025-01-13 22:57:49 -0300

    Update pessoa.py
    Gets e sets da class pessoa

--- pessoa.py
+++ pessoa.py
@@ -1,24 +1,38 @@
-class Pessoa():
-    def __init__(self, nome, sobrenome, idade, endereco):
+class Pessoa:
+    def __init__(self, nome: str, sobrenome: str, idade, endereco: str):
         self._nome = nome
         self._sobrenome = sobrenome
         self._idade = idade
         self._endereco = endereco
 
-
-
     @property
     def nome(self):
-        return self.nome
+        return self._nome
+
+    @nome.setter
+    def nome(self, valor: str):
+        self._nome = valor
 
     @property
     def sobrenome(self):
-        return self.sobrenome
+        return self._sobrenome
+
+    @sobrenome.setter
+    def sobrenome(self, valor: str):
+        self._sobrenome = valor
 
     @property
     def idade(self):
-        return self.__idade
+        return self._idade
 
+    @idade.setter
+    def idade(self, valor):
+        self._idade = valor
 
     @property
     def endereco(self):
+        return self._endereco
+
+    @endereco.setter
+    def endereco(self, valor: str):
+        self._endereco = valor
